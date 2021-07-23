859844612e57a5611155c1199926ff17528afbec
Georgios Nikolaidis <dev.georgios.nikolaidis@gmail.com>
2021-07-23 17:42:39 +0200

    add data from trending coins

--- src/components/Header/TrendingCoins/TrendingCoins.js
+++ src/components/Header/TrendingCoins/TrendingCoins.js
@@ -5,19 +5,22 @@ import { Link } from 'react-router-dom';
 
 const TrendingCoins = () => {
     const {trendingCoins} = useCoinContext();
-    const trendingCoinsArray = Object.entries(trendingCoins)
+
+    const trendingCoinsArray = trendingCoins.length > 0 && Object.entries(trendingCoins.coins);
     // console.log(trendingCoinsArray[0])
 
-    console.log(trendingCoins)
+    console.log(trendingCoinsArray)
     
 
-    const displayTrendingCoins = trendingCoinsArray.length > 0 ? trendingCoinsArray[0].map((coin)=>{
+    const displayTrendingCoins = trendingCoinsArray.length > 0 ? trendingCoinsArray.map((coin)=>{
         const { coin_id ,name,symbol,small} = coin;
+        // console.log(coin)
        
         return (
-                <Link to={`/${name}`} key={coin_id}>
-                    <img src={small} alt={`${name} image`} className="coin-image" />
-                </Link>
+                <div></div>
+                // <Link to={`/${name}`} key={coin_id}>
+                //     <img src={small} alt={`${name} image`} className="coin-image" />
+                // </Link>
              )
     }):[];
 
